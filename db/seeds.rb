50770c68b5a6bae81d91030fdb25ee8b58c24eb8
Taufiq Muhammadi <waruboy@yahoo.com>
2015-02-23 04:32:24 +0700

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Product.delete_all
+
+Product.create(title: 'Programming Ruby 1.9',
+  description:
+    %{<p>
+        Ruby is the fastes growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbos.
+      </p>},
+  image_url: 'ruby.jpg',
+  price: 49.95)
