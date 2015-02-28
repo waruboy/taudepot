a5f684bb005b66cb8e620d3be4b3b64b895e0633
Taufiq Muhammadi <waruboy@yahoo.com>
2015-02-28 12:52:55 +0700

    add current time

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'dt', 'Programming Ruby 1.9'
   end
 
   test "should get new" do
