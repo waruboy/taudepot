5b5cf43722e91eece7beb1a42f0d386897efe675
Taufiq Muhammadi <waruboy@yahoo.com>
2015-03-01 05:36:12 +0700

    add counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,18 @@
 class StoreController < ApplicationController
   def index
+    set_counter
+    if session[:counter] > 5
+      @counter = "You have visited this page %s" % view_context.pluralize(session[:counter], "time")
+    end
     @products = Product.order(:title)
   end
+
+  private
+    def set_counter
+      if session[:counter].nil?
+        session[:counter] = 1
+      else
+        session[:counter] += 1
+      end
+    end
 end
