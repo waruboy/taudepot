7877e79c909cf6ea81d7e02d78cd632f42d8c0a4
Taufiq Muhammadi <waruboy@yahoo.com>
2015-03-08 06:10:02 +0700

    order's atom feed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :line_items
+  has_many :orders, through: :line_items
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
@@ -7,12 +8,12 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true,
                     length: {
-                      minimum: 10, 
+                      minimum: 10,
                       message: 'must be at least 10 characters long'
                     }
   validates :image_url, allow_blank: true, format: {
     with:     %r{\.(gif|jpg|png)\Z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.' 
+    message: 'must be a URL for GIF, JPG or PNG image.'
   }
 
   def self.latest
