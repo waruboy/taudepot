b20d2a6c4cb8b07b3d33b1386c0f47f9409ae39c
Taufiq Muhammadi <waruboy@yahoo.com>
2015-03-17 05:29:36 +0700

    selecting locale

--- config/routes.rb
+++ config/routes.rb
@@ -11,24 +11,29 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
 
   resources :users
-
-  resources :orders
-
-  resources :line_items do
-    member do
-      post 'decrement'
-    end
-  end
-
-  resources :carts
-
-  get 'store/index'
-
   resources :products do
     get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+
+  scope '(:locale)' do
+    resources :orders
+
+    resources :line_items do
+      member do
+        post 'decrement'
+      end
+    end
+
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
+
+
+
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
